f293c93e8d5b0b624f4c35ec478ea5b6d8d07a02
Corey Osman <corey@nwops.io>
2019-08-26 15:20:41 -0400

    Fix bug with cloning private repositories
      * Without this change pdksync assumes all modules are
        public.  This fixes that by checking if the git url
        starts with a git@ pattern and generates a proper url
        like git@github.com:nwops/puppet-private.git instead of
        git@github.com/nwops/puppet-private.git.

--- lib/pdksync.rb
+++ lib/pdksync.rb
@@ -268,7 +268,10 @@ module PdkSync
   # @return [Git::Base]
   #   A git object representing the local repository.
   def self.clone_directory(namespace, module_name, output_path)
-    Git.clone("#{configuration.git_base_uri}/#{namespace}/#{module_name}.git", output_path.to_s) # is returned
+    # not all urls are public facing so we need to conditionally use the correct separator
+    sep = @git_base_uri.start_with?('git@') ? ':' : '/'
+    clone_url = "#{configuration.git_base_uri}#{sep}#{namespace}/#{module_name}.git"
+    Git.clone(clone_url, output_path.to_s) # is returned
   rescue Git::GitExecuteError => error
     puts "(FAILURE) Cloning #{module_name} has failed. #{error}".red
   end
